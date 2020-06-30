a4be2ae140822b5363949cd2b760a16970a4ecc3
Bryan Guillen <bryanguillen@Bryans-MacBook-Pro.local>
2020-06-29 19:35:12 -0600

    Finished User Controller and Routes

--- controllers/user.js
+++ controllers/user.js
@@ -6,6 +6,9 @@ const bcrypt = require('bcrypt-nodejs');
 const User = require('../models/user');
 //import jwt service file
 const jwt = require('../services/jwt');
+//import file system and path
+const fs = require('fs');
+const path = require('path');
 
 //EXPORT FUNCTION TO SIGN UP USER TO DATABASE	
 exports.saveUser = (req, res, next) => {
@@ -122,6 +125,72 @@ exports.updateUser = (req, res, next) => {
 				//return sucessful response and updated user
 				res.status(200).send({ user: updatedUser });
 			}
+		}
+	});
+}
+
+//method to uplaod user image
+exports.uploadImage = (req, res, next) => {
+	//retrieve user id from params
+	const userId = req.params.id;
+	//default file name
+	let file_name = 'Not uploaded...';
+
+	//check if there is a file coming in the requst
+	if (req.files) {
+		//retrieve file path from req
+		const file_path = req.files.image.path;
+		//split file path
+		const file_split = file_path.split('\\');
+		//get only name from split file name
+		const file_name = file_split[2];
+
+		//split on dot and get file extension
+		const ext_split = name.split('\.');
+		const file_ext = ext_split[1];
+
+		//check if file has correct extension
+		if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+			//update image on database using model
+			User.findByIdAndUpdate(userId, { image: file_name }, (err, updatedUser) => {
+				//if thre is an error with the returned user
+				if (!updatedUser) {
+					//return error response
+					res.status(404).send({ message: 'Could not update user' });
+				} else {
+					//return sucessful response and updated user
+					res.status(200).send({ user: updatedUser });
+				}
+			});
+		} else {
+			//return error response, wrong file extentsion
+			res.status(200).send({ message: 'File Extention is not valid...' });
+		}
+
+
+	} else {
+		//return error response
+		res.status(200).send({ message: 'No image was uploaded...' });
+	}
+}
+
+//method to get image files
+exports.getImageFile = (req, res, next) => {
+	//retrieve image file name from params
+	const imageFile = req.params.imageFile;
+	//entire file path
+	const file_path = `./uploads/users/${imageFile}`;
+
+	//check if file exists in folder
+	//callback to do something with it
+	fs.exists(file_path, exists => {
+		//if file exists
+		if (exists) {
+			//send the file
+			res.sendFile(path.resolve(file_path));
+		} else {
+			//return error response
+			res.status(200).send({ message: 'Image does not exist...' });
 		}
 	});
 }
--- routes/user.js
+++ routes/user.js
@@ -6,15 +6,28 @@ const UserController = require('../controllers/user');
 //import auth middlware
 const auth = require('../middlewares/auth');
 
+//for file upload 
+const multipart = require('connect-multiparty');
+//upload middlware
+const md_upload = multipart({ uploadDir: './uploads/users' });
+
 //router
 const api = express.Router();
 
 //route to register users
 api.post('/register', UserController.saveUser);
+
 //route for login users
 api.post('/login', UserController.loginUser);
+
 //route for updating a user
 api.put('/update-user/:id', auth.ensureAuth, UserController.updateUser);
+
+//route for uploading image
+api.post('/upload-image-user/:id', [auth.ensureAuth, md_upload], UserController.uploadImage);
+
+//route for getting files with correct path
+api.get('/get-image-user/:imageFile', UserController.getImageFile);
 
 //export routes
 module.exports = api;
