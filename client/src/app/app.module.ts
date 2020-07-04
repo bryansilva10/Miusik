f75f4459181f7822c2359961ed61b21b37722ddb
Bryan Guillen <bryanguillenlds@gmail.com>
2020-07-04 13:22:11 -0600

    Fixed Client folder

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
